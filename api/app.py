6c5fac8c371fc7fb16bf26905768d7bb3ef195fb
erimvelik <137645776+erimvelik@users.noreply.github.com>
2024-01-16 01:33:49 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,35 +1,5 @@
 from flask import Flask, render_template, request
-#from bs4 import BeautifulSoup
-#import string
-#from requests import get
-
 from api.util import playlist
-
-#def compute(date):
-#  date = date.split('/')
-#  
-#  program_year = date[2]
-#  program_month = date[0]
-#  program_day = date[1]
-#
-#  URL = "https://www.cbc.ca/listen/live-radio/1-1044-about-time/clip/d" +program_year + program_month + program_day 
-#
-#  page = get(URL)
-#
-#  soup = BeautifulSoup(page.content, "html.parser")
-#
-#  results = soup.find(id="app")
-#  
-#  job_elements = results.find_all("div", class_="ule-playlog__track__item__value")
-#
-#  track_list = []
-#  
-#  for job_element in job_elements:
-#    track_list.append(string.capwords(job_element.contents[0]))
-#  
-#  
-#  return track_list
-
 
 app = Flask(__name__)
 
