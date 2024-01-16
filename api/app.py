b9eb9bb818362a8cbf26082583c004ce3184a2dc
erimvelik <137645776+erimvelik@users.noreply.github.com>
2024-01-16 00:10:55 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+import playlist
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+def main_tom_allen():
+    return render_template("tomallen-date.html")
+
+@app.route('/tomallen-results', methods=['POST'])
+def tom_allen_results():
+    if request.method=='POST':
+      track_list = playlist.compute(date=request.form['date'])  
+      total_tracks = int(len(track_list) / 4)
+      
+      if (len(track_list) > 0):
+      #return job_elements[0]
+        return render_template("about_time.html", track_list=track_list, total_tracks=total_tracks)
+      else:
+        return "<h3>No Show Today!</h3>"
